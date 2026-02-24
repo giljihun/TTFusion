430f15feda3e61004b9b750594269d4b27096c4d
giljihun <accc45@outlook.com>
2026-02-24 13:04:29 +0900

    chore: project file 추가

---
+++ TTFusion/TTFusion/TTFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTFusionApp.swift
+//  TTFusion
+//
+//  Created by 길지훈 on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct TTFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
